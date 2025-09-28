ffece086ee5d2ac99d30627d8c89546f9aed06dc
iscmarkoantonio <iscmarko2021@gmail.com>
2025-09-28 01:03:28 -0600

    metodo update de propiedades

--- app/Livewire/Propiedads.php
+++ app/Livewire/Propiedads.php
@@ -13,7 +13,7 @@ class Propiedads extends Component
 
     protected $paginationTheme = 'tailwind';
 
-    //campos del formulario
+    //campos del formulario de creacion
 
     public $tipo;
     public $direccion;
@@ -21,8 +21,20 @@ class Propiedads extends Component
     public $descripcion;
     public $estado = 'disponible';
 
+    //campos del formulario de edicion
+
+    public $editTipo;
+    public $editDireccion;
+    public $editPrecio;
+    public $editDescripcion;
+    public $editEstado;
+
+    //control de modales
+    public $createModal = false;
     public $showModal = false;
+    public $editModal = false;
     public $propiedadSeleccionada;
+    public $propiedadEditando;
 
     //reglas de validacion
     protected $rules =[
@@ -33,16 +45,30 @@ class Propiedads extends Component
     'estado' => 'required|in:disponible,alquilado,mantenimiento',
     ];
 
-    //recetar campos del formulario
-    public function resetForm()
+    //resetear campos del formulario de creacion
+    public function resetCreateForm()
     {
-        $this->tipo = '';
-        $this->direccion = '';
-        $this->precio = '';
-        $this->descripcion = '';
-        $this->estado = '';
+        $this->reset(['tipo', 'direccion', 'precio', 'descripcion', 'estado']);
+        $this->resetErrorBag();
     }
 
+     //resetear campos del formulario de edicion
+    public function resetEditForm()
+    {
+        $this->reset(['editTipo', 'editDireccion', 'editPrecio', 'editDescripcion', 'editEstado', 'propiedadEditando']);
+        $this->resetErrorBag();
+    }
+
+    //abrir modal de creacion
+    public function openCreateModal()
+    {
+        $this->resetCreateForm();
+        // Flux::modal('crear-propiedad')->open();
+        $this->createModal = true;
+    }
+
+
+    //Guardar nueva propiedad
     public function save()
     {
         $this->validate();
@@ -54,8 +80,9 @@ class Propiedads extends Component
             'descripcion' => $this->descripcion,
             'estado' => $this->estado,
         ]);
-        $this->resetForm();
+        $this->resetCreateForm();
         Flux::modal('crear-propiedad')->close();
+        $this->createModal = false;
         session()->flash('message', 'Propiedad creada exitosamente.');
     }
 
@@ -63,6 +90,41 @@ class Propiedads extends Component
     {
         $this->propiedadSeleccionada = Propiedad::find($id);
         $this->showModal = true;
+    }
+
+    public function edit($id)
+    {
+        $this->propiedadEditando = Propiedad::find($id);
+        $this->editTipo = $this->propiedadEditando->tipo;
+        $this->editDireccion = $this->propiedadEditando->direccion;
+        $this->editPrecio = $this->propiedadEditando->precio;
+        $this->editDescripcion = $this->propiedadEditando->descripcion;
+        $this->editEstado = $this->propiedadEditando->estado;
+
+        $this->editModal = true;
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'editTipo' => 'required|string|max:255',
+            'editDireccion' => 'required|string|max:255',
+            'editPrecio' => 'required|numeric|min:0',
+            'editDescripcion' => 'required|string|max:1000',
+            'editEstado' => 'required|in:disponible,alquilado,mantenimiento',
+        ]);
+
+        $this->propiedadEditando->update([
+            'tipo' => $this->editTipo,
+            'direccion' => $this->editDireccion,
+            'precio' => $this->editPrecio,
+            'descripcion' => $this->editDescripcion,
+            'estado' => $this->editEstado,
+        ]);
+
+        $this->resetEditForm();
+        $this->editModal = false;
+        session()->flash('message', 'Propiedad actualizada exitosamente');
     }
 
     public function render()
--- resources/views/livewire/propiedads.blade.php
+++ resources/views/livewire/propiedads.blade.php
@@ -25,14 +25,81 @@
     <br>
     <br>
 
-
-    <flux:modal.trigger name="crear-propiedad">
-        <flux:button variant="primary">Crear nueva propiedad</flux:button>
-    </flux:modal.trigger>
+    <flux:button variant="primary" color="blue" wire:click="openCreateModal">Crear nueva propiedad</flux:button>
 
     <br><br>
 
-    <flux:modal name="crear-propiedad" class="md:w-96" style="width: 600px">
+    {{-- @if (session('message'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+            <strong class="font-bold">Exito!</strong>
+            <span class="block sm:inline">{{ session('message') }}</span>
+        </div>
+    @endif --}}
+
+
+    <div class="overflow-hidden w-full overflow-x-auto rounded-radius border border-outline dark:border-outline-dark">
+        <table class="w-full text-left text-sm text-on-surface dark:text-on-surface-dark">
+            <thead
+                class="border-b border-outline bg-surface-alt text-sm text-on-surface-strong dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark-strong text-center">
+                <tr>
+                    <th scope="col" class="p-4">Nro</th>
+                    <th scope="col" class="p-4">Tipo</th>
+                    <th scope="col" class="p-4">Dirección</th>
+                    <th scope="col" class="p-4">Precio</th>
+                    <th scope="col" class="p-4">Estado</th>
+                    <th scope="col" class="p-4">Acciones</th>
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-outline dark:divide-outline-dark">
+                @foreach ($propiedades as $propiedad)
+                    <tr>
+                        <td class="p-4 text-center">{{ $propiedad->id }}</td>
+                        <td class="p-4">{{ $propiedad->tipo }}</td>
+                        <td class="p-4">{{ $propiedad->direccion }}</td>
+                        <td class="p-4">{{ $propiedad->precio }}</td>
+                        <td class="p-4">{{ $propiedad->estado }}</td>
+                        <td class="p-4">
+                            <!-- primary Button Show with Icon -->
+                            <button wire:click="show({{ $propiedad->id }})"
+                                class="inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-radius bg-primary border border-primary dark:border-primary-dark px-4 py-2 text-sm font-medium tracking-wide text-on-primary transition hover:opacity-75 text-center focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed dark:bg-primary-dark dark:text-on-primary-dark dark:focus-visible:outline-primary-dark">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="lucide lucide-eye-icon lucide-eye">
+                                    <path
+                                        d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+                                    <circle cx="12" cy="12" r="3" />
+                                </svg>
+                                Ver
+                            </button>
+
+                            <!-- primary Button Edit with Icon -->
+                            <button wire:click="edit({{ $propiedad->id }})"
+                                class="inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-radius bg-surface-alt border border-surface-alt dark:border-surface-dark-alt px-4 py-2 text-xs font-medium tracking-wide text-on-surface-strong transition hover:opacity-75 text-center focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-surface-alt active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed dark:bg-surface-dark-alt dark:text-on-surface-dark-strong dark:focus-visible:outline-surface-dark-alt">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                </svg>
+                                Edit
+                            </button>
+
+                            {{-- <flux:button wire:click="show({{ $propiedad->id }})">ver</flux:button> --}}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="mt-4">
+            {{ $propiedades->links() }}
+        </div>
+    </div>
+
+
+
+    {{-- Modal para la vista create propiedad --}}
+    <flux:modal name="create-propiedad" class="md:w-96" style="width: 600px" wire:model="createModal">
         <div class="space-y-6">
             <div>
                 <flux:heading size="lg">Crear nuevo.</flux:heading>
@@ -71,14 +138,13 @@
                     @enderror
                 </div>
 
-
                 <div>
                     <flux:label for="estado">Estado <b>(*)</b></flux:label>
                     <flux:select id="estado" wire:model="estado">
                         <option value="">Seleccione un estado</option>
                         <option value="disponible">Disponible</option>
-                        <option value="vendido">Vendido</option>
                         <option value="alquilado">Alquilado</option>
+                        <option value="mantenimiento">Mantenimiento</option>
                     </flux:select>
                     @error('estado')
                         <span class="tex-red-500">{{ $message }}</span>
@@ -95,85 +161,8 @@
                     <flux:button type="submit" variant="primary">Crear Propiedad</flux:button>
                 </div>
             </form>
-
         </div>
-
     </flux:modal>
-
-
-    {{-- @if (session('message'))
-        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
-            <strong class="font-bold">Exito!</strong>
-            <span class="block sm:inline">{{ session('message') }}</span>
-        </div>
-    @endif --}}
-
-
-    @if (session('message'))
-        <div x-data x-init="Swal.fire({
-            icon: 'success',
-            title: '{{ session('message') }}d',
-            showConfirmButton: false,
-            timer: 1500
-        })">
-
-        </div>
-    @endif
-
-
-
-
-
-    <div class="overflow-hidden w-full overflow-x-auto rounded-radius border border-outline dark:border-outline-dark">
-        <table class="w-full text-left text-sm text-on-surface dark:text-on-surface-dark">
-            <thead
-                class="border-b border-outline bg-surface-alt text-sm text-on-surface-strong dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark-strong text-center">
-                <tr>
-                    <th scope="col" class="p-4">Nro</th>
-                    <th scope="col" class="p-4">Tipo</th>
-                    <th scope="col" class="p-4">Dirección</th>
-                    <th scope="col" class="p-4">Precio</th>
-                    <th scope="col" class="p-4">Estado</th>
-                    <th scope="col" class="p-4">Acciones</th>
-                </tr>
-            </thead>
-            <tbody class="divide-y divide-outline dark:divide-outline-dark">
-                @foreach ($propiedades as $propiedad)
-                    <tr>
-                        <td class="p-4 text-center">{{ $propiedad->id }}</td>
-                        <td class="p-4">{{ $propiedad->tipo }}</td>
-                        <td class="p-4">{{ $propiedad->direccion }}</td>
-                        <td class="p-4">{{ $propiedad->precio }}</td>
-                        <td class="p-4">{{ $propiedad->estado }}</td>
-                        <td>
-                            <!-- primary Button with Icon -->
-                            <button wire:click="show({{ $propiedad->id }})"
-                                class="inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-radius bg-primary border border-primary dark:border-primary-dark px-4 py-2 text-sm font-medium tracking-wide text-on-primary transition hover:opacity-75 text-center focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed dark:bg-primary-dark dark:text-on-primary-dark dark:focus-visible:outline-primary-dark">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round"
-                                    class="lucide lucide-eye-icon lucide-eye">
-                                    <path
-                                        d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
-                                    <circle cx="12" cy="12" r="3" />
-                                </svg>
-                                Ver
-                            </button>
-
-                            {{-- <flux:button wire:click="show({{ $propiedad->id }})">ver</flux:button> --}}
-                        </td>
-                    </tr>
-                @endforeach
-
-
-
-            </tbody>
-        </table>
-
-        <div class="mt-4">
-            {{ $propiedades->links() }}
-        </div>
-    </div>
 
 
 
@@ -225,4 +214,87 @@
             @endif
         </div>
     </flux:modal>
+
+
+    {{-- Modal para la vista edicion propiedad --}}
+    <flux:modal name="editar-propiedad" class="md:w-96" style="width: 600px" wire:model="editModal">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Editar propiedad</flux:heading>
+                <flux:text class="mt-2">Llene todos los campos requeridos.</flux:text>
+            </div>
+            <form wire:submit.prevent="update" class="space-y-4">
+                <div>
+                    <flux:label for="tipo">Tipo de propiedad <b>(*)</b></flux:label>
+                    <flux:input type="text" id="tipo" wire:model="editTipo" required />
+                    @error('tipo')
+                        <span class="tex-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <flux:label for="direccion">Dirección <b>(*)</b></flux:label>
+                    <flux:input type="text" id="direccion" wire:model="editDireccion" required />
+                    @error('direccion')
+                        <span class="tex-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <flux:label for="precio">Precio <b>(*)</b></flux:label>
+                    <flux:input type="number" id="precio" wire:model="editPrecio" required />
+                    @error('precio')
+                        <span class="tex-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <flux:label for="descripcion">Descripcion <b>(*)</b></flux:label>
+                    <flux:textarea id="descripcion" wire:model="editDescripcion" rows="3"></flux:textarea>
+                    @error('descripcion')
+                        <span class="tex-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <flux:label for="estado">Estado <b>(*)</b></flux:label>
+                    <flux:select id="estado" wire:model="editEstado">
+                        <option value="">Seleccione un estado</option>
+                        <option value="disponible">Disponible</option>
+                        <option value="alquilado">Alquilado</option>
+                        <option value="mantenimiento">Mantenimiento</option>
+                    </flux:select>
+                    @error('estado')
+                        <span class="tex-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <br>
+                <br>
+
+                <div class="flex justify-end">
+                    <flux:modal.close name="editar-propiedad" class="mr-2">
+                        <flux:button type="button" variant="filled">Cerrar</flux:button>
+                    </flux:modal.close>
+                    <flux:button type="submit" variant="primary">Actualizar Propiedad</flux:button>
+                </div>
+            </form>
+        </div>
+    </flux:modal>
+
+
+
+    @if (session('message'))
+        <div x-data x-init="Swal.fire({
+            icon: 'success',
+            title: '{{ session('message') }}',
+            showConfirmButton: false,
+            timer: 1500
+        })">
+
+        </div>
+    @endif
+
+
+
 </div>
